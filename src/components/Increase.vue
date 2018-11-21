0dec15cd06a03d14326334a8509a9d2f3225fad7
Cyril <contact@cnicodeme.com>
2018-11-21 14:35:13 +0100

    Changing credits to conversions in increase page

--- src/components/Increase.vue
+++ src/components/Increase.vue
@@ -5,7 +5,7 @@
                 <header-component />
                 <h1>
                     Get more free conversions
-                    <small>You can do these points to get more free credits</small>
+                    <small>You can do these points to get more free conversions</small>
                 </h1>
             </div>
         </header>
@@ -13,7 +13,7 @@
         <div class="container">
             <div class="description">
                 <p>
-                    You can do the following options to get more free credits.<br />
+                    You can do the following options to get more free conversions.<br />
                     For each actions, please <strong>send us either a screenshot or a link</strong> and we will update your account accordingly.
                 </p>
             </div>
@@ -21,31 +21,31 @@
                 <li>
                     <div>
                         Talk about us on Twitter (<a href="http://bit.ly/pdfshift-on-twitter" target="_blank">Here's a quick tweet if you like</a>)
-                        <strong>+50 credits</strong>
+                        <strong>+50 conversions</strong>
                     </div>
                 </li>
                 <li>
                     <div>
                         Review us on <a href="https://alternativeto.net/software/pdfshift/" target="_blank">AlternativeTo</a>
-                        <strong>+100 credits</strong>
+                        <strong>+100 conversions</strong>
                     </div>
                 </li>
                 <li>
                     <div>
                         Add your review on <a href="https://www.g2crowd.com/products/pdfshift/reviews" target="_blank">G2Crowd</a>
-                        <strong>+100 credits</strong>
+                        <strong>+100 conversions</strong>
                     </div>
                 </li>
                 <li>
                     <div>
                         Write a blog post talking about PDFShift
-                        <strong>+350 credits</strong>
+                        <strong>+350 conversions</strong>
                     </div>
                 </li>
                 <li>
                     <div>
                         Mention PDFShift in the footer of your website
-                        <strong>+150 credits</strong>
+                        <strong>+150 conversions</strong>
                     </div>
                 </li>
             </ul>
