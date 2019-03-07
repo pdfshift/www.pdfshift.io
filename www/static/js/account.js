3c444d6c872ea774fc2bb9e79a8c65f9c48095a8
Cyril <contact@cnicodeme.com>
2019-03-07 09:07:24 +0100

    Updating rewrites url

--- www/static/js/account.js
+++ www/static/js/account.js
@@ -10,6 +10,33 @@
             if ('setAccount' in window.PDFShift) {
                 window.PDFShift.setAccount(body);
             }
+            if (window.Vitally) {
+                Vitally.account({
+                    'accountId': token,
+                    'traits': {
+                        'name': body.name
+                    }
+                });
+
+                var traits = {
+                    'name': body.name,
+                    'email': body.email,
+                    'createdAt': body.created,
+                    'country': body.country
+                };
+
+                if (body.plan) {
+                    traits['planId'] = body.plan.id;
+                    traits['planName'] = body.plan.display;
+                    traits['planYearly'] = body.plan.yearly;
+                }
+                
+                Vitally.user({
+                    'userId': token,
+                    'accountId': token,
+                    'traits': traits
+                });
+            }
         },
         function (response) {
             if (response.status === 0) {
