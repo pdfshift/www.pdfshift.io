510610e4c8356853136479b6687425fd4f218b3b
Cyril <contact@cnicodeme.com>
2019-04-15 11:25:00 +0200

    Pushing new prices and improving UI for guides

--- gulpfile.js
+++ gulpfile.js
@@ -210,8 +210,8 @@ gulp.task('serve', gulp.series('clean', 'sass', 'css', 'nunjucks', 'js', 'static
             baseDir: './dist'
         },
         open: false,
-        port: 8080
-        // host: '77.193.92.35'
+        port: 8080,
+        host: '86.77.97.2'
     });
 }));
 
